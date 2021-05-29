b6bfedafc0887a3873519c7828596e76d3e248e8
Jeisson Osorio <jeisson.osoriob@gmail.com>
2021-05-29 10:56:48 -0500

    ADD: se carga proyecto haciendo uso de registry publico propio

---
+++ interact-with-terraform-modules/using-owned-module-from-registry/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+}
+
+module "tags" {
+  source = "github.com/Tanok-Technologies/playground.git/modules/aws/commons/tags"
+}
+
+// Module documentation
+// https://registry.terraform.io/modules/JeissonO/modules/aws/latest
+module "modules" {
+  source   = "JeissonO/modules/aws"
+  version  = "0.0.3"
+  s3       = true
+  resource = "test"
+  tags     = module.tags.tags_commons
+}
