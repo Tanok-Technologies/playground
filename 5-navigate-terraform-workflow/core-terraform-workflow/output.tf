38ed45d2d66b2e198e709a8dff10a493160e21e3
Juan Diego <jadegbonp@gmail.com>
2021-05-31 20:09:21 -0500

    Ajuste a outputs del modulo de navigate-terraform-workflow

--- 5-navigate-terraform-workflow/core-terraform-workflow/output.tf
+++ 5-navigate-terraform-workflow/core-terraform-workflow/output.tf
@@ -1,9 +1,9 @@
 # Salidas del stack
 output "instance_public_ip" {
-  value       = module.compute.web.public_ip
+  value       = module.compute.instance_public_ip
   description = "Ip publica de la instancia"
 }
 output "instance_public_dns" {
-  value       = module.compute.public_dns
+  value       = module.compute.instance_public_dns
   description = "Dms publico de la instancia"
 }
