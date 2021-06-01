ba73988d7222f7debafdf3e307fe8cbf831b7460
Cristhian lopez <cristhianrich1104@gmail.com>
2021-06-01 08:57:26 -0500

    Se renombra el folder

---
+++ 3-use-the-terraform-CLI/import-to-module/modules/iam/variables.tf
@@ -0,0 +1,11 @@
+variable "user_name" {
+    description = "nombre del usuario"
+    type = string 
+}
+variable "group_name" {
+    description = "nombre del grupo"
+    type = string
+}
+variable "tags" {
+
+}
