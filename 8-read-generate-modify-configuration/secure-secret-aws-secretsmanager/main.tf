bc227be592f3408d7a701b34d340f6a9ddc8e135
Jeisson Osorio <jeisson.osoriob@gmail.com>
2021-06-01 19:31:43 -0500

    ADD: Se agregan pruebas con vault

---
+++ 8-read-generate-modify-configuration/secure-secret-aws-secretsmanager/main.tf
@@ -0,0 +1,17 @@
+/**
+  @Autor: jeisson.osoriob@gmail.com
+  @Date: Junio 2021
+  @Organization: Tanok Tech
+  @Description: Template de prueba para uso de AWS Secrets Manager   
+**/
+provider "aws" {}
+
+// Inicio uso de AWS Secrets Manager
+// Para esta prueba en AWS Secrets managger se creo un secreto con el id (poc-tanok-secret) 
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "poc-tanok-secret"
+}
+output "secret_output" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["password"]
+}
+// Fin uso de AWS Secrets Manager
---
+++ 8-read-generate-modify-configuration/secure-secret-vault/main.tf
@@ -0,0 +1,31 @@
+/**
+  @Autor: jeisson.osoriob@gmail.com
+  @Date: Junio 2021
+  @Organization: Tanok Tech
+  @Description: Template de prueba para uso de HashiCorp Vault
+**/
+
+
+// Inicio uso de HashisCorp Vault
+// Para esta prueba en Vault existe un secret engine con el path (aws/secrets) y un secreto con el nombre (tanok)
+// vault server -dev
+// vault server -dev -dev-root-token-id="estudio"
+/* 
+export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_TOKEN="estudio"
+*/
+provider "vault" {}
+
+data "vault_generic_secret" "tanok_secrets" {
+  path = "tanok/secrets/bd"
+}
+output "vault_user" {
+  value = data.vault_generic_secret.tanok_secrets.data["user"]
+  sensitive = true
+}
+output "vault_pass" {
+  value = data.vault_generic_secret.tanok_secrets.data["pass"]
+  sensitive = true
+}
+// terraform output -json
+// Fin Vault
