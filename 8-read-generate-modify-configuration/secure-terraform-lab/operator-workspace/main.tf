59dd84caa0523bfc84f68eca055a87db2482ac90
Jeisson Osorio <jeisson.osoriob@gmail.com>
2021-06-02 10:22:50 -0500

    ADD: Se agregan archivos de laboratorio de terraform sobre seguridad

---
+++ 8-read-generate-modify-configuration/secure-terraform-lab/operator-workspace/main.tf
@@ -0,0 +1,44 @@
+variable "name" { default = "dynamic-aws-creds-operator" }
+variable "region" { default = "us-east-2" }
+variable "path" { default = "../vault-admin-workspace/terraform.tfstate" }
+variable "ttl" { default = "1" }
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "admin" {
+  backend = "local"
+
+  config = {
+    path = var.path
+  }
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = data.terraform_remote_state.admin.outputs.backend
+  role    = data.terraform_remote_state.admin.outputs.role   
+  type    = "sts" 
+  ttl     = 3600
+  role_arn = "arn:aws:iam::506080924848:role/default-terraform-role"
+}
+
+provider "aws" {
+  region     = var.region
+  assume_role {
+    role_arn = "arn:aws:iam::506080924848:role/default-terraform-role"
+  }
+  //access_key = data.vault_aws_access_credentials.creds.access_key
+  //secret_key = data.vault_aws_access_credentials.creds.secret_key
+}
+
+
+module "tags" {
+  source = "github.com/Tanok-Technologies/playground.git/modules/aws/commons/tags"
+}
+module "bucket_s3" {
+  source = "github.com/Tanok-Technologies/playground.git/modules/aws/storage/s3"
+  tags   = module.tags.tags_commons
+}
---
+++ 8-read-generate-modify-configuration/secure-terraform-lab/vault-admin-workspace/main.tf
@@ -0,0 +1,51 @@
+//variable "aws_access_key" {}
+//variable "aws_secret_key" {}
+variable "name" { default = "dynamic-aws-creds-vault-admin" }
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+provider "vault" {}
+
+resource "vault_aws_secret_backend" "aws" {
+  //access_key = var.aws_access_key
+  //secret_key = var.aws_secret_key
+  path       = "${var.name}-path"
+
+  default_lease_ttl_seconds = "900"
+  max_lease_ttl_seconds     = "900"
+}
+
+resource "vault_aws_secret_backend_role" "admin" {
+  backend         = vault_aws_secret_backend.aws.path
+  name            = "${var.name}-role"
+  //credential_type = "iam_user"
+  credential_type = "assumed_role"
+  role_arns       = ["arn:aws:iam::506080924848:role/default-terraform-role"]
+
+  policy_document = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:*", "s3:*", "sts:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+output "backend" {
+  value = vault_aws_secret_backend.aws.path
+}
+
+output "role" {
+  value = vault_aws_secret_backend_role.admin.name
+}
