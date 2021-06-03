a6f03a443608034533b29dc7e56ca6361f2f76dd
Jeisson Osorio <jeisson.osoriob@gmail.com>
2021-06-03 16:02:45 -0500

    ADD: Example para uso de Dynamic Blocks

---
+++ 2-understand-terraform-basics/dynamic-blocks/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {}
+
+locals {
+  ingress = [
+    {
+      from = 443
+      to   = 443
+    },
+    {
+      from = 80
+      to   = 80
+    },
+    {
+      from = 90
+      to   = 90
+    }
+  ]
+}
+variable "vpc_id" {
+  description = "Identificador de la vpc en la cual se creara el SG"
+  type = string 
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "dynamic_block_sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = local.ingress
+    //iterator = ite
+    content {
+      description = ""
+      //from_port   = ite.value.from // value, key
+      //to_port     = ite.value.to
+      from_port   = ingress.value.from // value, key
+      to_port     = ingress.value.to
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+}
+
+
+
+ 
